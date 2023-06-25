483322237d297e09389be46386addf8f6e97cc73
lawson1000 <lawsonolanrewaju@yahoo.com>
2023-06-25 13:21:05 +0100

    Allow user to pick a range of guess they want

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -5,42 +5,60 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GuessTheNumber {
-	int randomNumber;
-	int numberGuessed;
+	int randomNumber, numberGuessed, max;
 	int numOfAttempt = 0;
+	boolean keepTrying = true;
+
+	void rangeOfNumber() {
+		Scanner roc = new Scanner(System.in);
+		while (keepTrying) {
+			System.out.println("Enter the range of Number you'll like to guess(1 - ???): ");
+			try {
+				max = roc.nextInt(); // Accepting User's Range
+			} catch (InputMismatchException e) {
+				System.err.println("Intergers Only");
+				roc.next();
+				continue;
+			}
+			if (max <= 10) { // Ensuring user range is from 10 above
+				System.err.println("Too Small, Enter Only Numbers Above 10");
+				continue;
+			}
+			keepTrying = false;
+		}
+	}
 
 	void generateRandom() {
 		Random rd = new Random();
-		randomNumber = rd.nextInt(1, 81); // Generating Random number from 1 - 80
-		System.out.println(randomNumber);
+		randomNumber = rd.nextInt(1, max + 1); // Generating Random number from 1 - user's choice
+//		System.out.println(randomNumber);
 	}
 
 	void userGuess() {
-		boolean keepTrying = true;
-		Scanner sc = new Scanner(System.in);
+		keepTrying = true;
+		Scanner ug = new Scanner(System.in);
 
 		while (keepTrying) {
 			numOfAttempt++; // counting the number of Attempts
-			System.out.println("Guess the Number(1-80): ");
+			System.out.printf("Guess the Number(1-%d): \n", max);
 			try {
-				numberGuessed = sc.nextInt(); // Accepting User's Guess
+				numberGuessed = ug.nextInt(); // Accepting User's Guess
 			} catch (InputMismatchException e) {
 				System.err.println("Intergers Only");
-				sc.next();
+				ug.next();
 				numOfAttempt--; // not counting invalid inputs as Attempt
 				continue;
 			}
 
-			if (numberGuessed > 80 || numberGuessed < 1) { // ensuring the User's guess is between 1-80
-				System.err.println("Enter between 1-80");
+			if (numberGuessed > max || numberGuessed < 1) { // ensuring the User's guess is between 1 - user's choice
+				System.out.printf("Enter between 1 - %d \n", max);
 				numOfAttempt--; // not counting invalid inputs as Attempt
 			} else if (numberGuessed < randomNumber) { //
 				System.out.println("Too Low!");
 			} else if (numberGuessed > randomNumber) {
 				System.out.println("Too High!");
 			} else {
-				System.out.println("Bravo! You are Correct \n"
-						+ "The Number is: " + randomNumber
+				System.out.println("Bravo! You are Correct \n" + "The Number is: " + randomNumber
 						+ "\nNumber of Attempt(s): " + numOfAttempt);
 				keepTrying = false; // break out of the loop
 			}
@@ -49,6 +67,7 @@ public class GuessTheNumber {
 
 	public static void main(String[] args) {
 		GuessTheNumber obj = new GuessTheNumber();
+		obj.rangeOfNumber();
 		obj.generateRandom();
 		obj.userGuess();
 	}
