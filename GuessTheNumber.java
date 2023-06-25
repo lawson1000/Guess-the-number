3fc22e5a5651c93bd718084db3a59c3f15baa5cf
lawson1000 <lawsonolanrewaju@yahoo.com>
2023-06-25 12:12:29 +0100

    Added Error Handling, ensuring the program do not end even if the user enter an invalid Input

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -1,5 +1,6 @@
 package chapter2;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -21,18 +22,27 @@ public class GuessTheNumber {
 		while (keepTrying) {
 			numOfAttempt++; // counting the number of Attempts
 			System.out.println("Guess the Number(1-80): ");
-			numberGuessed = sc.nextInt(); // Accepting User's Guess
+			try {
+				numberGuessed = sc.nextInt(); // Accepting User's Guess
+			} catch (InputMismatchException e) {
+				System.err.println("Intergers Only");
+				sc.next();
+				numOfAttempt--; // not counting invalid inputs as Attempt
+				continue;
+			}
 
 			if (numberGuessed > 80 || numberGuessed < 1) { // ensuring the User's guess is between 1-80
 				System.err.println("Enter between 1-80");
+				numOfAttempt--; // not counting invalid inputs as Attempt
 			} else if (numberGuessed < randomNumber) { //
 				System.out.println("Too Low!");
 			} else if (numberGuessed > randomNumber) {
 				System.out.println("Too High!");
 			} else {
-				System.out.println("Correct \n"
-						+ "Number of Attempt(s): " + numOfAttempt );
-				keepTrying = false;
+				System.out.println("Bravo! You are Correct \n"
+						+ "The Number is: " + randomNumber
+						+ "\nNumber of Attempt(s): " + numOfAttempt);
+				keepTrying = false; // break out of the loop
 			}
 		}
 	}
