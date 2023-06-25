e084440c772c95bd50eb050cdf3d9de6f22614e6
lawson1000 <lawsonolanrewaju@yahoo.com>
2023-06-25 10:34:02 +0100

    generated Random Number and matched it with User's Guess

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -1,9 +1,39 @@
 package chapter2;
+import java.util.Random;
+import java.util.Scanner;
 
 public class GuessTheNumber {
+	int rNum;
+	int numberGuessed;
+	
+	void generateRandom() {
+		Random rd = new Random();
+		rNum = rd.nextInt(1,81); // Generating Random number from 1 - 80 
+		System.out.println(rNum);
+	}
+	void userGuess() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Guess the Number: ");
+		numberGuessed = sc.nextInt(); 
+		
+		if (numberGuessed < rNum) {
+			System.out.println("Too Low!");
+		}
+		else if(numberGuessed > rNum) {
+			System.out.println("Too High!");
+		}
+		else {
+			System.out.println("Correct");
+		}
+	}
 
 	public static void main(String[] args) {
-
+		GuessTheNumber obj = new GuessTheNumber();
+		obj.generateRandom();
+		obj.userGuess();
+		
+		
+		Scanner sc = new Scanner(System.in);
 	}
 
 }
