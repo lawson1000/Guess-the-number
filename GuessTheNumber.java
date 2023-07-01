15d3af843f2eaa92d326f599bf7e4e857b79698b
lawson1000 <lawsonolanrewaju@yahoo.com>
2023-07-01 17:27:45 +0100

    Added Tries and game Over

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -5,11 +5,13 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GuessTheNumber {
-	int randomNumber, numberGuessed, max;
+
+	int randomNumber, numberGuessed, max, tries;
 	int numOfAttempt = 0;
 	boolean keepTrying = true;
 
 	void rangeOfNumber() {
+
 		Scanner roc = new Scanner(System.in);
 		while (keepTrying) {
 			System.out.println("Enter the range of Number you'll like to guess(1 - ???): ");
@@ -28,6 +30,28 @@ public class GuessTheNumber {
 		}
 	}
 
+	int scoreBoard() {
+		if (max < 30) {
+			tries = 5;
+			return tries;
+		}
+		if (max < 100) {
+			tries = 8;
+			return tries;
+		}
+		if (max < 250) {
+			tries = 10;
+			return tries;
+		}
+		if (max < 500) {
+			tries = 12;
+			return tries;
+		} else
+			tries = 15;
+
+		return tries;
+	}
+
 	void generateRandom() {
 		Random rd = new Random();
 		randomNumber = rd.nextInt(1, max + 1); // Generating Random number from 1 - user's choice
@@ -35,12 +59,15 @@ public class GuessTheNumber {
 	}
 
 	void userGuess() {
+
 		keepTrying = true;
 		Scanner ug = new Scanner(System.in);
+		System.out.println("\nSTART GAME!!! ");
 
 		while (keepTrying) {
+
 			numOfAttempt++; // counting the number of Attempts
-			System.out.printf("Guess the Number(1-%d): \n", max);
+			System.out.printf("\nNumber of tries left : %d \n" + "Guess the Number(1-%d): \n", tries, max);
 			try {
 				numberGuessed = ug.nextInt(); // Accepting User's Guess
 			} catch (InputMismatchException e) {
@@ -55,21 +82,51 @@ public class GuessTheNumber {
 				numOfAttempt--; // not counting invalid inputs as Attempt
 			} else if (numberGuessed < randomNumber) { //
 				System.out.println("Too Low!");
+				tries--; // reducing the number of tries left
 			} else if (numberGuessed > randomNumber) {
 				System.out.println("Too High!");
+				tries--; // reducing the number of tries left
 			} else {
 				System.out.println("Bravo! You are Correct \n" + "The Number is: " + randomNumber
 						+ "\nNumber of Attempt(s): " + numOfAttempt);
 				keepTrying = false; // break out of the loop
 			}
+
+			if (tries == 0) {
+				gameOver();
+				break;
+			}
+
 		}
 	}
 
+	void gameOver() {
+		System.out.printf("\nYou Lost! Game Over!!!  \n"
+				+ "Play Again? Enter Y/N: ");
+		Scanner go = new Scanner(System.in);
+		String userTryAgain = go.nextLine();
+		if (userTryAgain.equalsIgnoreCase("Y")){
+			rangeOfNumber();
+			scoreBoard();
+			generateRandom();
+			userGuess();
+
+		}
+		else if (userTryAgain.equalsIgnoreCase("N")){
+			System.out.println("BYE!!!");
+		}
+	}
+	
+	
+
 	public static void main(String[] args) {
+
 		GuessTheNumber obj = new GuessTheNumber();
 		obj.rangeOfNumber();
+		obj.scoreBoard();
 		obj.generateRandom();
 		obj.userGuess();
+
 	}
 
 }
